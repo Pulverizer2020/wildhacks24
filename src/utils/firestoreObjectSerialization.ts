b7191771016b0310ff86b9aea0b6348583033b9a
Andrew Pulver <64996411+Pulverizer2020@users.noreply.github.com>
2024-04-07 03:21:55 -0500

    added map editing, exporting to database, loading from database

---
+++ src/utils/firestoreObjectSerialization.ts
@@ -0,0 +1,61 @@
+// firestore objects can't have directly nested arrays
+
+type NormalFeaturesObjectPartial = {
+  features: {
+    geometry: {
+      coordinates: [number, number][][];
+      type: "Polygon" | "LineString";
+    };
+  }[];
+};
+type FirebaseFeaturesObjectPartial = {
+  features: {
+    geometry: { coordinates: number[]; type: "Polygon" | "LineString" };
+  }[];
+};
+
+// for map features
+export const normalFeaturesObjectToFirebaseFeaturesObject = (
+  normalFeaturesObject: NormalFeaturesObjectPartial
+): FirebaseFeaturesObjectPartial => {
+  let firebaseFeaturesObject: FirebaseFeaturesObjectPartial =
+    normalFeaturesObject;
+
+  for (let i = 0; i < firebaseFeaturesObject.features.length; i++) {
+    firebaseFeaturesObject.features[i].geometry.coordinates =
+      normalFeaturesObject.features[i].geometry.coordinates.flat(2);
+  }
+
+  return firebaseFeaturesObject;
+};
+
+// for map features
+export const firebaseFeaturesObjectToNormalFeaturesObject = (
+  firebaseFeaturesObject: FirebaseFeaturesObjectPartial
+): NormalFeaturesObjectPartial => {
+  let normalFeaturesObject: NormalFeaturesObjectPartial =
+    firebaseFeaturesObject;
+
+  for (let i = 0; i < normalFeaturesObject.features.length; i++) {
+    normalFeaturesObject.features[i].geometry.coordinates = groupIntoPairs(
+      firebaseFeaturesObject.features[i].geometry.coordinates
+    );
+
+    // add another layer of nested array
+    if (normalFeaturesObject.features[i].geometry.type === "Polygon") {
+      normalFeaturesObject.features[i].geometry.coordinates = [
+        normalFeaturesObject.features[i].geometry.coordinates,
+      ];
+    }
+  }
+
+  return normalFeaturesObject;
+};
+
+function groupIntoPairs(numbers: number[]) {
+  const pairs = [];
+  for (let i = 0; i < numbers.length; i += 2) {
+    pairs.push([numbers[i], numbers[i + 1]]);
+  }
+  return pairs;
+}
