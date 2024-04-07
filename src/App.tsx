7b8f342440bb9db8b0dbabe43d9ab8d46b4fa065
Andrew Pulver <64996411+Pulverizer2020@users.noreply.github.com>
2024-04-07 04:38:21 -0500

    added zoom and center... and upload confirmation modal

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import {
   firebaseFeaturesObjectToNormalFeaturesObject,
   normalFeaturesObjectToFirebaseFeaturesObject,
 } from "./utils/firestoreObjectSerialization";
+import Swal from "sweetalert2";
 
 const config = {
   apiKey: "AIzaSyDrkCcTA-x2YRWDg9irkpp41YfJ7Nllo1U",
@@ -86,18 +87,36 @@ function App() {
     console.log("formattedShapes", mapState);
 
     setDoc(docRef, mapState, { merge: true })
-      .then(() => console.log("Document successfully written!"))
+      .then(() => {
+        console.log("Document successfully written!");
+        Swal.fire({
+          title: "Upload Successful",
+          html: `
+          Here is the sharable link for your map: 
+          <a style="color: #60a5fa;" href=${
+            import.meta.env.VITE_HOST
+          }/#${smallUUID}>
+            ${import.meta.env.VITE_HOST}/#${smallUUID}
+          </a>
+          `,
+          icon: "success",
+        });
+      })
       .catch((error) => console.error("Error writing document: ", error));
   };
 
   return (
     <>
       <SearchBar iframeRef={iframeRef} />
-      {uuid && <p>Your URL: http://localhost:5173/#{uuid}</p>}
+      {uuid && (
+        <p>
+          Your URL: {import.meta.env.VITE_HOST}/#{uuid}
+        </p>
+      )}
 
       <iframe
         ref={iframeRef}
-        style={{ width: "100%", height: "500px" }}
+        style={{ width: "100%", height: "80vh" }}
         src="map.html"
       />
     </>
