cb40da566205df24fbf2b422b49809d68cd3e630
wildlynx424 <>
2024-04-06 20:39:46 -0500

    Fixed capitalization and added the ability to press enter to search.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import SearchBar from "./components/Searchbar";
+import SearchBar from "./components/searchbar";
 
 function App() {
   return (
     <>
       <SearchBar />
-      <iframe style={{ width: "100%", height: "500px" }} src="map.html" />
     </>
   );
 }
