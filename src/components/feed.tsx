c6fde57ab8024dc9d2992987dce130e772637f51
Ryan Newkirk <ryannewkirk2024@u.northwestern.edu>
2024-04-07 08:20:13 -0500

    Update feed.tsx

--- src/components/feed.tsx
+++ src/components/feed.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getFirestore, doc, setDoc, getDoc, getDocs, addDoc, collection } from "firebase/firestore";
 import { nanoid } from 'nanoid';
+import Swal from "sweetalert2";
 
 const db = getFirestore();
 
@@ -21,6 +22,93 @@ function Feed() {
 
         fetchPosts();
     }, []);
+
+    const handleSubmitComment = (comment) => {
+        console.log("TODO: submit comment: " + comment);
+
+    };
+
+    const handleShowComments = (comments, title) => {
+        const html = comments.map(comment =>
+            `<div >
+                  <article class="p-6 mb-3 text-base bg-white border-b border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+                  <footer class="flex justify-between items-center mb-2">
+                      <div class="flex items-center">
+                          <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold"><img
+                                  class="mr-2 w-8 h-8 rounded-full"
+                                  src="${comment.profilePicUrl}"
+                                  alt="${comment.username}">${comment.username}</p>
+                          <p class="text-sm text-gray-600 dark:text-gray-400"><time pubdate datetime="2022-03-12"
+                                  title="March 12th, 2022">Mar. 12, 2022</time></p>
+                      </div>
+                      <button id="dropdownComment3Button" data-dropdown-toggle="dropdownComment3"
+                          class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-40 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                          type="button">
+                          <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
+                              <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
+                          </svg>
+                          <span class="sr-only">Comment settings</span>
+                      </button>
+                      <!-- Dropdown menu -->
+                      <div id="dropdownComment3"
+                          class="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                          <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
+                              aria-labelledby="dropdownMenuIconHorizontalButton">
+                              <li>
+                                  <a href="#"
+                                      class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
+                              </li>
+                              <li>
+                                  <a href="#"
+                                      class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Remove</a>
+                              </li>
+                              <li>
+                                  <a href="#"
+                                      class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Report</a>
+                              </li>
+                          </ul>
+                      </div>
+                  </footer>
+                  <p class="text-left text-gray-600">${comment.comment}</p>
+                  
+              </article>
+                </div>`
+        ).join('');
+        Swal.fire({
+            html: `<div class="flex justify-between items-center mb-6">
+            <h2 class="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">Comments</h2>
+        </div>` + html + `<div class="max-w-2xl mx-auto px-4">
+            <div class="mb-6">
+                <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+                    <label for="comment" class="sr-only">Your comment</label>
+                    <textarea id="comment" rows="6" class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800" placeholder="Write a comment..." required></textarea>
+                </div>
+            </div>
+        </div>`,
+            width: 800,
+            showCancelButton: true,
+            confirmButtonText: 'Post comment',
+            focusConfirm: false,
+            didOpen: () => {
+                const popup = Swal.getPopup()!
+                const commentInput = popup.querySelector('#comment') as HTMLInputElement
+                commentInput.focus()
+                commentInput.onkeyup = (event) => event.key === 'Enter' && Swal.clickConfirm()
+            },
+            preConfirm: () => {
+                const comment = (document.getElementById('comment') as HTMLInputElement).value
+                if (!comment) {
+                    Swal.showValidationMessage(`Please enter a comment`)
+                }
+                return comment
+            },
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const comment = result.value
+                handleSubmitComment(comment)
+            }
+        });
+    };
 
     const handleNewPost = async () => {
         const postID = nanoid(); // Generate a unique ID for the post
@@ -88,7 +176,7 @@ function Feed() {
                                         <path fill-rule="evenodd" clip-rule="evenodd" d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 13.5997 2.37562 15.1116 3.04346 16.4525C3.22094 16.8088 3.28001 17.2161 3.17712 17.6006L2.58151 19.8267C2.32295 20.793 3.20701 21.677 4.17335 21.4185L6.39939 20.8229C6.78393 20.72 7.19121 20.7791 7.54753 20.9565C8.88837 21.6244 10.4003 22 12 22ZM8 13.25C7.58579 13.25 7.25 13.5858 7.25 14C7.25 14.4142 7.58579 14.75 8 14.75H13.5C13.9142 14.75 14.25 14.4142 14.25 14C14.25 13.5858 13.9142 13.25 13.5 13.25H8ZM7.25 10.5C7.25 10.0858 7.58579 9.75 8 9.75H16C16.4142 9.75 16.75 10.0858 16.75 10.5C16.75 10.9142 16.4142 11.25 16 11.25H8C7.58579 11.25 7.25 10.9142 7.25 10.5Z"></path>
                                     </g>
                                 </svg>
-                                <span>{post.comments} Comments</span>
+                                <span>{post.comments.length} Comments</span>
                             </button>
 
                             <button class="flex p-2.5 bg-yellow-500 rounded-xl hover:rounded-3xl hover:bg-yellow-600 transition-all duration-300 text-white">
@@ -112,7 +200,9 @@ function Feed() {
                                 {post.description}
                             </div>
                         </div>
-                        <div className="text-sm mb-2 text-gray-500 cursor-pointer font-medium">View all 14 comments</div>
+                        <div className="text-sm mb-2 text-gray-500 cursor-pointer font-medium">
+                            <button onClick={() => handleShowComments(post.comments, post.title)}>View all {post.comments.length} comments</button>
+                        </div>
                         {/*
                             <div className="mb-2">
                                 <div className="mb-2 text-sm">
