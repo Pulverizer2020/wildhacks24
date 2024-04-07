48603b3ed4f6baa2c3c1b174da72223b60779a62
Andrew Pulver <64996411+Pulverizer2020@users.noreply.github.com>
2024-04-07 11:23:48 -0500

    maybe map will work in prod

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,7 +10,7 @@ function Navbar() {
     logout();
   };
 
-  const [toggleBurger, setToggleBurger] = useState(true); // you could default to false
+  const [toggleBurger, setToggleBurger] = useState(false);
 
   return (
     <>
